108d8cd959239e6295ec672230b25eb37eb0089b
rusty <robert.strauss@protonmail.com>
2020-08-04 19:25:27 -0600

    made event listener to convert new cells to nodes

--- main.js
+++ main.js
@@ -1,68 +1,6 @@
 define(['base/js/namespace','base/js/events', 'require'], function(Jupyter, events, requirejs) {
 
 
-/**
-
-
-function makeDraggable(node) {
-    let deltax = 0, deltay = 0, x1 = 0, y1 = 0;
-    //   if (document.getElementById(node.id + "header")) {
-    //     // if present, the header is where you move the DIV from:
-    //     document.getElementById(node.id + "header").onmousedown = dragMouseDown;
-    //   } else {
-    //     // otherwise, move the DIV from anywhere inside the DIV:
-    //     node.onmousedown = dragMouseDown;
-    //   }
-    node.onmousedown = dragMouseDown;
-    node.style.position = 'absolute';
-    node.style.width = '500px';
-    node.style.resizable = 'both';
-    node.style.background = 'white';
-    node.style.zIndex = 1;
-    node.style.boxShadow = "black 0px 0px 10px 0px";
-
-    function dragMouseDown(e) {
-        e = e || window.event;
-        node.click(); // select node
-        // bring element to top when clicked
-        const parent = node.parentElement; // get elements parent
-        node.remove(); // remove element
-        parent.append(node); // add it back, to the top
-        if (e.target.nodeName == 'PRE' || e.target.nodeName == 'SPAN') {
-        // dont activate dragging if user is selecting text
-        } else {
-            e.preventDefault();
-            // get the mouse cursor position at startup:
-            x1 = e.clientX;
-            y1 = e.clientY;
-            document.onmouseup = closeDragElement;
-            // call a function whenever the cursor moves:
-            document.onmousemove = elementDrag;
-        }
-    }
-
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        deltax = x1 - e.clientX;
-        deltay = y1 - e.clientY;
-        x1 = e.clientX;
-        y1 = e.clientY;
-        // set the element's new position:
-        node.style.top = (node.offsetTop - deltay) + "px";
-        node.style.left = (node.offsetLeft - deltax) + "px";
-    }
-
-    function closeDragElement() {
-        // stop moving when mouse button is released:
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-}
-
-
-*/
 
 
 
@@ -103,73 +41,25 @@ function addToolMenu(node) {
 
     function hideToolMenu(e) {
         $('#maintoolbar').css('display', 'none');
-        //         document.onmouseup = null;
     }
 }
 
 
 
 
+// converts a standard jupyter cell (DOM element) to a node with full functionality
+function cellToNode(cell) {
+    // make cell draggable and resiazble
+    $(cell).resizable();
+    $(cell).draggable();
 
-/**
-
-
-function getCoords(elem) { // crossbrowser coord finder from stackoverflow
-    var box = elem.getBoundingClientRect();
-
-    var body = document.body;
-    var docEl = document.documentElement;
-
-    var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
-    var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
-
-    var clientTop = docEl.clientTop || body.clientTop || 0;
-    var clientLeft = docEl.clientLeft || body.clientLeft || 0;
-
-    var top  = box.top +  scrollTop - clientTop;
-    var left = box.left + scrollLeft - clientLeft;
-
-    return [top, left];
-}
-
-*/
-
-
-
-// attach custom stylesheet
-$('<link/>').attr('type', 'text/css').attr('rel', 'stylesheet').attr('href', requirejs.toUrl('./style.css')).appendTo('head');
-
-
-// add jquery ui
-//$('body').append('<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>');
-$('<script>').attr('src', "https://code.jquery.com/ui/1.12.1/jquery-ui.js").appendTo('body');
-
-// make cells movable and resizable
-$('.cell').draggable();
-$('.cell').resizable();
-
-
-// make floating cells opaque and in the same plane
-//$('.cell').css('background', 'white');
-//$('.cell').css('z-index', 1);
-//$('.cell').css('box-shadow', "black 0px 0px 5px 0px"); // add box shadow
-//$('.cell').css('width', '500px');
-// $('.cell').css('transition', '0s');
-
-
-
-//$('#notebook-container').css('transition', '1s');
-//$('#notebook-container').css('background', 'transparent');
-//$('#notebook-container').css('box-shadow', 'none');
-
-
-// add pins, change context menu, and add focusing functionality to existing nodes
-for (cell of $('.cell').get().reverse()) {
+    // add ui elements to cell
     addPins(cell);
     addToolMenu(cell);
 
-    const top  = cell.offsetTop;//getBoundingClientRect().top;
-    const left = cell.offsetLeft;//getBoundingClientRect().left;
+    // keep position when converting to focusable
+    const top  = cell.offsetTop;
+    const left = cell.offsetLeft;
     const width = cell.offsetWidth;
     $(cell).css('position', 'absolute');
     $(cell).css('top',   top);
@@ -182,6 +72,26 @@ for (cell of $('.cell').get().reverse()) {
         this.click();
     });
 }
+
+
+// attach custom stylesheet
+$('<link/>').attr('type', 'text/css').attr('rel', 'stylesheet').attr('href', requirejs.toUrl('./style.css')).appendTo('head');
+
+
+// add jquery ui
+$('<script>').attr('src', "https://code.jquery.com/ui/1.12.1/jquery-ui.js").appendTo('body');
+
+
+// convert every existing cell to a node
+for (cell of $('.cell').get().reverse()) {
+    cellToNode(cell);
+}
+
+// convert created cells to nodes
+console.log(Jupyter.notebook.events.bind);
+Jupyter.notebook.events.on('create.Cell', (event, data)=>{
+    cellToNode(data.cell.element[0]);
+});
 
 
 
