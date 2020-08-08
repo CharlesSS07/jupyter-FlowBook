6ea7f4d0db4beb49cba63d6285fbda6615a52d78
CharlesSS07 <charles.s.strauss@gmail.com>
2020-08-08 18:52:40 -0400

    created node bakcend and automaitc jupyter installer

---
+++ cell-node.js
@@ -0,0 +1,206 @@
+
+class VarSpace {
+
+	static c = 0;
+	static global_space_var = "NodeSpace";
+	static get_varname() {
+		VarSpace.c+=1;
+		return VarSpace.global_space_var+"["+VarSpace.c+"]"; // get the variable contained by the nodespace variable on the python side
+	}
+}
+
+class FuncSpace {
+
+	static c = 0;
+        static global_space_var = "FuncSpace";
+        static get_funcname() {
+                FuncSpace.c+=1;
+                return FuncSpace.global_space_var+".func_"+FuncSpace.c; // get the variable contained by the nodespace variable on the python side
+        }
+}
+
+class Wire {
+
+
+	constructor(from, address) {
+		this.from = from; // NodeOutput
+		this.address = address;
+	}
+
+	getOutputVar() {
+		if (this.from==null) {
+			return this.address.getName();
+		}
+		return this.from.variableName;
+	}
+
+	updateAddress(address) {
+		this.address = address;
+	}
+
+	getFrom() {
+		return this.from;
+	}
+}
+
+class NodeInputAddress {
+
+	constructor(input, name) {
+		this.input = input;
+		this.name = name;
+	}
+
+	getInput() {
+		return this.input;
+	}
+
+	getName() {
+		return this.name;
+	}
+}
+
+class NodeInput {
+
+	constructor() {
+		this.inputs = {};
+	}
+
+	nextVarName() {
+		// find a variable that has not been used as input yet
+		// loop through this list, and then start adding _{c} to generate new names
+		var default_var_names = ['x', 'y', 'z', 'a', 'b', 'c', 'o', 'p', 'q', 'w'];
+		var default_var_names_cache = JSON.parse(JSON.stringify(default_var_names));
+		var name = default_var_names.shift();
+		var c = 0;
+		for (var k in this.inputs) {
+			if (name==k) {
+				if (default_var_names.length>=0) {
+					name = default_var_names.shift();
+				} else {
+					default_var_names = JSON.parse(JSON.stringify(default_var_names_cache));
+					c+=1;
+					for (var i in default_var_names) {
+						i+="_"+c;
+					}
+				}
+			}
+		}
+	}
+
+	onAddInput() {
+		var name = this.nextVarName();
+		this.inputs[name] = new Wire(null, this, name);
+	}
+	onRemoveInput(name) {
+		delete this.inputs[name];
+	}
+	onSetInputName(oldName, newName) {
+		// set the variable name of the input
+		tmp = this.inputs[oldName];
+		delete this.inputs[oldName];
+		this.inputs[newName] = tmp;
+		this.inputs[newName].updateAddress(new NodeInputAddress(this, name));
+	}
+	onSetInputWire(name, wire) {
+		this.inputs[name] = wire;
+		this.inputs[name].updateName(name);
+		this.inputs[name].getFrom().addUser(new NodeInputAddress(this, name));
+	}
+	onRemoveWire(name) {
+		this.inputs[name] = new Wire(null, this, name);
+	}
+}
+
+class NodeOutput {
+
+	constructor() {
+		// tells python to put all output generated from this code into this variable
+		this.varableName = VarSpace.get_varname();
+		this.users = [];
+	}
+
+	addUser(user) {
+		this.users.push(user);
+	}
+
+	getUsers() {
+		return this.users;
+	}
+}
+
+class NodeData {
+
+	constructor(type) {
+		//this.metadata??
+		this.inputs = new NodeInput();
+		this.output = new NodeOutput(); // NodeOutput
+		this.type = type; // things that are specific to this nodes type (function, title)
+	}
+
+	getInputs() {
+		return this.inputs;
+	}
+
+	getOutput() {
+		return this.output;
+	}
+
+	setType(type) {
+		delete this.type;
+		this.type = type;
+	}
+}
+
+class NodeType {
+
+	constructor(title, cell) {
+		this.cell = cell; // cell to display, function to run on
+		this.title = title; // title of this cell
+		if (this.title==null) {
+			this.title = FuncSpace.get_funcname();
+			this.named = false;
+		}
+	}
+
+	setTitle(title) {
+		this.title = title;
+		this.named = true;
+	}
+
+	getTitle() {
+		return title;
+	}
+
+	getCell() {
+		return cell;
+	}
+
+}
+
+class NodeManager {
+
+	constructor() {
+		this.nodes = [];
+		this.types = {};
+	}
+
+	newNode(cell) {
+		var type = this.newType(new NodeType(null, cell));
+		var newnode = new NodeData(type);
+		this.nodes.push(newnode);
+		return newnode;
+	}
+
+	newType(type) {
+		if (type in this.types) {
+                        this.types[type]+= 1;
+                } else {
+                        this.types[type] = 1;
+                }
+		return type;
+	}
+
+	getNodes() {
+		return this.nodes;
+	}
+}
